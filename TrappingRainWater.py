cd95f85be66fba04d172c432e96dc35ed9b37bdb
arshadahmed <arshadahmedk0306@gmail.com>
2025-08-19 15:51:03 +0530

    1

--- TrappingRainWater.py
+++ TrappingRainWater.py
@@ -19,9 +19,7 @@ def trap(height):
 
     return water
 
-
-# Run and test
 if __name__ == "__main__":
     height = [0,1,0,2,1,0,1,3,2,1,2,1]
     print("Height map:", height)
-    print("Trapped Water:", trap(height))  # Expected Output: 6
+    print("Trapped Water:", trap(height))
