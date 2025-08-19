eb1bdac44b4a08dd90af44326b6fa701ba1a72c0
arshadahmed <arshadahmedk0306@gmail.com>
2025-08-19 15:50:30 +0530

    1

--- BalancedSubArray.py
+++ BalancedSubArray.py
@@ -1,13 +1,29 @@
+# def longest_equal_boys_girls(s: str) -> int:
+#     prefix_sum = 0
+#     seen = {0: -1}
+#     max_len = 0
+#
+#     for i, ch in enumerate(s):
+#         prefix_sum += 1 if ch == 'B' else -1
+#
+#         if prefix_sum in seen:
+#             max_len = max(max_len, i - seen[prefix_sum])
+#         else:
+#             seen[prefix_sum] = i
+#
+#     return max_len
+#
+# line = "BBGGGB"
+# print(longest_equal_boys_girls(line))
+
 def longest_equal_boys_girls(s: str) -> int:
+    vowels=['a', 'e', 'i', 'o', 'u']
     prefix_sum = 0
     seen = {0: -1}
     max_len = 0
 
     for i, ch in enumerate(s):
-        if ch == 'B':
-            prefix_sum += 1
-        elif ch == 'G':
-            prefix_sum -= 1
+        prefix_sum += 1 if ch.lower() in vowels else -1
 
         if prefix_sum in seen:
             max_len = max(max_len, i - seen[prefix_sum])
@@ -16,5 +32,5 @@ def longest_equal_boys_girls(s: str) -> int:
 
     return max_len
 
-line = "BBGGGB"
+line = "ABABGGGBEAEAE"
 print(longest_equal_boys_girls(line))
