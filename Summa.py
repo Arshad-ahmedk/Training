bd872fe910db2072fb414b3170ef5976f68b1f1e
arshadahmed <arshadahmedk0306@gmail.com>
2025-08-22 10:07:38 +0530

    1

--- Summa.py
+++ Summa.py
@@ -1,33 +1,18 @@
-def solveNQueens(n):
-    solutions = []
+class Solution:
+    def canSeePersonsCount(self, heights: List[int]) -> List[int]:
+        n = len(heights)
+        res = [0] * n
+        stack = []
 
-    rowf = [False for _ in range(n)]
-    colf = [False for _ in range(n)]
-    t1d = [False for _ in range(2*n+1)]
-    t2d = [False for _ in range(2*n+1)]
-    board = [["."]*n for _ in range(n)]
+        for i in range(n - 1, -1, -1):  # iterate from right to left
+            count = 0
+            while stack and stack[-1] < heights[i]:
+                stack.pop()
+                count += 1
+            if stack:  # can also see the next taller person
+                count += 1
+            res[i] = count
+            stack.append(heights[i])
 
-    def fill(row):
-        if row == n:
-            solutions.append(["".join(r) for r in board])
-            return
+        return res
 
-        for col in range(n):
-            if rowf[row] or colf[col] or t1d[row + col] or t2d[row - col + n]:
-                continue
-            board[row][col] = "Q"
-            rowf[row] = colf[col] = t1d[row + col] = t2d[row - col + n] = True
-
-            fill(row + 1)
-
-            board[row][col] = "."
-            rowf[row] = colf[col] = t1d[row + col] = t2d[row - col + n] = False
-
-    fill(0)
-    return solutions
-
-res = solveNQueens(4)
-for solution in res:
-    for row in solution:
-        print(row)
-    print()
---
+++ VisiblePeopleInQueue(LC1944).py
@@ -0,0 +1,36 @@
+# heights = [10,6,8,5,11,9]
+# n=len(heights)
+# counts=[]
+# for h in range(n):
+#     a=heights.pop(0)
+#     max = 0
+#     count = 0
+#     for h2 in heights:
+#         if a>h2 and max<h2:
+#             count+=1
+#             max=h2
+#         elif a<h2:
+#             count+=1
+#             break
+#     counts.append(count)
+# print(counts)
+# #TIME LIMIT EXCEEDED
+
+class Solution:
+    def canSeePersonsCount(self, heights):
+        n = len(heights)
+        res = [-1] * n
+        stack = []
+        for i in range(n - 1, -1, -1):
+            count = 0
+            while stack and stack[-1] <= heights[i]:
+                stack.pop()
+                count += 1
+            if stack:
+                count += 1
+
+            res[i] = count
+            stack.append(heights[i])
+
+        return res
+print(Solution().canSeePersonsCount([10,6,8,5,11,9]))
